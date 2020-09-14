7bcc55e1e674b4d0c6706fc4d49e5cda1362bb39
Olivia <olivia@macbook-pro.myfiosgateway.com>
2020-09-14 19:01:24 -0400

    Project React Router First Commitment MVP Almost Finished

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -1,16 +1,20 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import { useParams } from 'react-router-dom'
 
 export default function Movie(props) {
   const [movie, setMovie] = useState();
 
-  let id = 1;
+  const { movieId } = useParams()
+  
   // Change ^^^ that line and use a hook to obtain the :id parameter from the URL
 
   useEffect(() => {
+    const id = movieId;
     axios
       .get(`http://localhost:5000/api/movies/${id}`) // Study this endpoint with Postman
       .then(response => {
+        setMovie(response.data)
         // Study this response with a breakpoint or log statements
         // and set the response data as the 'movie' slice of state
       })
@@ -19,10 +23,14 @@ export default function Movie(props) {
       });
     // This effect should run every time time
     // the `id` changes... How could we do this?
-  }, []);
+  }, [movieId]);
 
   // Uncomment this only when you have moved on to the stretch goals
-  // const saveMovie = evt => { }
+  const saveMovie = evt => {
+    const addToSavedList = props.addToSavedList;
+    addToSavedList(movie)
+   }
+
 
   if (!movie) {
     return <div>Loading movie information...</div>;
@@ -48,7 +56,7 @@ export default function Movie(props) {
           </div>
         ))}
       </div>
-      <div className="save-button">Save</div>
+      <div onClick= {saveMovie} className="save-button">Save</div>
     </div>
   );
 }
--- client/src/Movies/MovieList.js
+++ client/src/Movies/MovieList.js
@@ -1,17 +1,24 @@
 import React from 'react';
+import { Link } from 'react-router-dom'
+import { useRouteMatch } from "react-router-dom"
 
 export default function MovieList(props) {
+  const { movies } =props
+  const { url } = useRouteMatch()
+
   return (
     <div className="movie-list">
-      {props.movies.map(movie => (
-        <MovieDetails key={movie.id} movie={movie} />
+      {movies.map(movie => (
+        <Link to={`${url}/${movie.id}`}>
+        < MovieDetails key={movie.id} movie={movie} />
+        </Link>
       ))}
     </div>
   );
 }
 
-function MovieDetails(props) {
-  const { title, director, metascore } = props.movie;
+function MovieDetails({ movie }) {
+  const { title, director, metascore } = movie;
 
   return (
     <div className="movie-card">
